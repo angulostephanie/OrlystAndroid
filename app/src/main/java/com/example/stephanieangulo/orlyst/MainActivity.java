54bc46df823353482b224f384093033d63396e4a
Stephanie Angulo <angulostephanie21@gmail.com>
2018-04-01 16:18:01 -0700

    made signup page, need to enable autho in firebase console

--- app/src/main/java/com/example/stephanieangulo/orlyst/MainActivity.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/MainActivity.java
@@ -1,69 +1,75 @@
 package com.example.stephanieangulo.orlyst;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
     EditText emailText;
     EditText passwordText;
     Button loginBtn;
+    Button signUpPageBtn;
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
         mAuth = FirebaseAuth.getInstance();
 
         emailText = findViewById(R.id.emailText);
         passwordText = findViewById(R.id.passwordText);
         loginBtn = findViewById(R.id.loginBtn);
+        signUpPageBtn = findViewById(R.id.signUpPageBtn);
+
+        signUpPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpPageIntent = new Intent(mContext, SignUpPageActivity.class);
+                startActivity(signUpPageIntent);
+            }
+        });
+
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 String email = emailText.getText().toString();
                 String password = passwordText.getText().toString();
                 if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password))
                     System.out.print("tf no");
                 else {
-                    Intent newsFeedIntent = new Intent()
-                    mAuth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d(TAG, "createUserWithEmail:success");
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                        //updateUI(user);
-                                    } else {
-                                        // If sign in fails, display a message to the user.
+                    final Intent newsFeedIntent = new Intent(mContext, NewsFeedActivity.class);
 
-                                        Log.w(, "createUserWithEmail:failure", task.getException());
-                                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                                Toast.LENGTH_SHORT).show();
-                                        //updateUI(null);
-                                    }
-
-                                    // ...
-                                }
-                            });
+//                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+//                                // Sign in success, update UI with the signed-in user's information
+//                                Log.d(TAG, "createUserWithEmail:success");
+//                                FirebaseUser user = mAuth.getCurrentUser();
+//                                startActivity(newsFeedIntent);
+//                                //updateUI(user);
+//                            } else {
+//                                // If sign in fails, display a message to the user.
+//                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                                Toast.makeText(mContext, "Authentication failed.",
+//                                        Toast.LENGTH_SHORT).show();
+//                                //updateUI(null);
+//                            }
+//                        }
+//                    });
                 }
 
             }
---
+++ app/src/main/java/com/example/stephanieangulo/orlyst/SignUpPageActivity.java
@@ -0,0 +1,73 @@
+package com.example.stephanieangulo.orlyst;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpPageActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpPageActivity";
+    private FirebaseAuth mAuth;
+    EditText emailText;
+    EditText passwordText;
+    Button signUpBtn;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+
+        mContext = this;
+        mAuth = FirebaseAuth.getInstance();
+
+        emailText = findViewById(R.id.newEmailText);
+        passwordText = findViewById(R.id.newPasswordText);
+        signUpBtn = findViewById(R.id.signUpPageBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+                if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password))
+                    System.out.print("tf no");
+                else {
+                    final Intent newsFeedIntent = new Intent(mContext, NewsFeedActivity.class);
+
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startActivity(newsFeedIntent);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(mContext, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
