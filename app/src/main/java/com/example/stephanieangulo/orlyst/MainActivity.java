79b7d09e94ca8fc449796021798591d67d4e1c9b
Stephanie Angulo <angulostephanie21@gmail.com>
2018-04-01 16:31:05 -0700

    lit, sign up and login work

--- app/src/main/java/com/example/stephanieangulo/orlyst/MainActivity.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.stephanieangulo.orlyst;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -51,27 +57,24 @@ public class MainActivity extends AppCompatActivity {
                     System.out.print("tf no");
                 else {
                     final Intent newsFeedIntent = new Intent(mContext, NewsFeedActivity.class);
-
-//                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//                            if (task.isSuccessful()) {
-//                                // Sign in success, update UI with the signed-in user's information
-//                                Log.d(TAG, "createUserWithEmail:success");
-//                                FirebaseUser user = mAuth.getCurrentUser();
-//                                startActivity(newsFeedIntent);
-//                                //updateUI(user);
-//                            } else {
-//                                // If sign in fails, display a message to the user.
-//                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                                Toast.makeText(mContext, "Authentication failed.",
-//                                        Toast.LENGTH_SHORT).show();
-//                                //updateUI(null);
-//                            }
-//                        }
-//                    });
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        startActivity(newsFeedIntent);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(mContext, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
-
             }
         });
 
@@ -83,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         System.out.print(currentUser);
-        //updateUI(currentUser);
     }
 
 
