56f9e291ba268fb3384aa56e9b3c35a7ca072d2a
Stephanie Angulo <angulostephanie21@gmail.com>
2018-04-02 10:57:04 -0700

    Can take image and pass image to new activity woo

---
+++ app/src/main/java/com/example/stephanieangulo/orlyst/AddItemInfoActivity.java
@@ -0,0 +1,41 @@
+package com.example.stephanieangulo.orlyst;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class AddItemInfoActivity extends AppCompatActivity {
+    Context mContext;
+    Button postBtn;
+    Button backBtn;
+    ImageView itemImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_info);
+        mContext = this;
+        postBtn = findViewById(R.id.post_btn);
+        backBtn = findViewById(R.id.back_btn);
+        itemImage = findViewById(R.id.item_image);
+
+        if(getIntent().hasExtra("bytes")) {
+            byte[] bytes = this.getIntent().getByteArrayExtra("bytes");
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            itemImage.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 120, 120, false));
+        }
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, TakePhotoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
