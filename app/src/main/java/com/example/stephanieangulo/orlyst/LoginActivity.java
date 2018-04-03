c803af7a900f503bc0ea492f5024734b7359d2cf
Stephanie Angulo <angulostephanie21@gmail.com>
2018-04-02 18:02:54 -0700

    Changed launcher activity to login, created updateUI method

--- app/src/main/java/com/example/stephanieangulo/orlyst/LoginActivity.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/LoginActivity.java
@@ -19,18 +19,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "LoginActivity";
     private FirebaseAuth mAuth;
-    EditText emailText;
-    EditText passwordText;
-    Button loginBtn;
-    Button signUpPageBtn;
-    Context mContext;
+    private EditText emailText;
+    private EditText passwordText;
+    private Button loginBtn;
+    private Button signUpPageBtn;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         mContext = this;
         mAuth = FirebaseAuth.getInstance();
 
@@ -38,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.password_text);
         loginBtn = findViewById(R.id.login_btn);
         signUpPageBtn = findViewById(R.id.sign_up_page_btn);
+
+        updateUI();
 
         signUpPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +87,20 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        System.out.print(currentUser);
+        Log.d(TAG,"HELLO THERE " + currentUser);
+        updateUI();
+    }
+
+    public void updateUI() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Log.d(TAG, "There is a user signed in!");
+            Intent intent = new Intent(mContext, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "No user signed in");
+            // No user is signed in
+        }
     }
 
 
