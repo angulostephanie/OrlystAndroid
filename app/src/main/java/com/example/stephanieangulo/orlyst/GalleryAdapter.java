0c08631efa3a4e6551c8af7344deadd47848ed0e
Stephanie Angulo <angulostephanie21@gmail.com>
2018-04-16 16:49:46 -0700

    scaled down bitmaps

--- app/src/main/java/com/example/stephanieangulo/orlyst/GalleryAdapter.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/GalleryAdapter.java
@@ -1,6 +1,9 @@
 package com.example.stephanieangulo.orlyst;
 
 import android.content.Context;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,33 +11,44 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder>{
+    private final String orderBy = MediaStore.Images.Media._ID;
+    private final String[] projection = {
+            MediaStore.Images.Media.DATA,
+            MediaStore.Images.Media._ID
+    };
+    private final Uri imageGalleryLink = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
     private Context context;
-    private List<byte[]> imageGallery;
-    public GalleryAdapter(Context context, List<byte[]> imageGallery) {
+    private static List<byte[]> imageGallery;
+
+    public GalleryAdapter(Context context) {
         this.context = context;
-        this.imageGallery = imageGallery;
+        this.imageGallery = ItemImage.getAllShownImagePaths(context,
+                imageGalleryLink, projection, orderBy);
     }
+
     @Override
-    public GalleryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public GalleryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.image_gallery_item, parent, false);
         return new GalleryAdapter.ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(GalleryAdapter.ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull GalleryAdapter.ViewHolder holder, final int position) {
         final byte[] bytes = imageGallery.get(position);
         Glide.with(context)
                 .asBitmap()
                 .load(bytes)
                 .apply(new RequestOptions()
                 .placeholder(R.drawable.loading_spinner)
-                        .fitCenter())
+                        .fitCenter()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL))
                 .into(holder.thumbnail);
     }
 
@@ -43,9 +57,13 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         return imageGallery.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView thumbnail;
-        public ViewHolder(View view) {
+    public static List<byte[]> getImageGallery() {
+        return imageGallery;
+    }
+
+     class ViewHolder extends RecyclerView.ViewHolder {
+         ImageView thumbnail;
+         ViewHolder(View view) {
             super(view);
             thumbnail = view.findViewById(R.id.image_gallery_image_view);
         }
--- app/src/main/java/com/example/stephanieangulo/orlyst/ItemImage.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/ItemImage.java
@@ -1,16 +1,26 @@
 package com.example.stephanieangulo.orlyst;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 // A helper class to put all image related functions in one place. :)
 public class ItemImage {
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 400;
     private byte[] bytes;
     private Bitmap bitmap;
     private String path;
 
+    public ItemImage() {}
     public ItemImage(byte[] bytes) {
         this.bytes = bytes;
     }
@@ -30,11 +40,20 @@ public class ItemImage {
             decodedBitmap = BitmapFactory.decodeFile(path);
         }
 
-        Bitmap mutableBitmap = decodedBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        return scaleDownBitmap(decodedBitmap);
+    }
+    public static Bitmap scaleDownBitmap(Bitmap originalBitmap) {
+        Bitmap mutableBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
 
         // return scaled version of bitmap (has to be scaled)
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(mutableBitmap, mutableBitmap.getWidth(),
-            mutableBitmap.getHeight(), false);
+        int width = mutableBitmap.getWidth();
+        int height = mutableBitmap.getHeight();
+        while(width > MAX_WIDTH || height > MAX_HEIGHT) {
+            width /=2;
+            height /=2;
+        }
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(mutableBitmap, width,
+                height, false);
         return scaledBitmap;
     }
     public byte[] convertToBytes(int quality) {
@@ -44,6 +63,33 @@ public class ItemImage {
     }
     public byte[] getBytes() {
         return bytes;
+    }
+
+    public static List<byte[]> getAllShownImagePaths(Context context,
+                                                      Uri imageGalleryLink,
+                                                      String[] projection,
+                                                      String orderBy) {
+        List<byte[]> list = new ArrayList<>();
+
+        ContentResolver imageGallery = context.getContentResolver();
+        Cursor cursor = imageGallery.query(imageGalleryLink,
+                projection, null, null, orderBy);
+
+        int index;
+        String path = null;
+        index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+
+
+        while (cursor.moveToNext()) {
+            path = cursor.getString(index);
+            ItemImage image = new ItemImage(path);
+            Bitmap bitmap = image.decodeToBitmap();
+            byte[] b = new ItemImage(bitmap).convertToBytes(100);
+            list.add(b);
+        }
+        cursor.close();
+
+        return list;
     }
 
 
