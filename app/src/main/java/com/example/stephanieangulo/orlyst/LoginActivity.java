b85ade0a91e34773d1bc0b31f058232b3f2ef598
Stephanie Angulo <angulostephanie21@gmail.com>
2018-05-06 17:50:23 -0700

    Added splash screen

--- app/src/main/java/com/example/stephanieangulo/orlyst/LoginActivity.java
+++ app/src/main/java/com/example/stephanieangulo/orlyst/LoginActivity.java
@@ -48,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.DarkTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mContext = this;
